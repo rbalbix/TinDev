d864c50e164c416be3e61b57e7e59095ae3f7c07
Ricardo Balbi <rbalbi@gmail.com>
2020-02-08 18:44:03 -0300

    feat(front): finish main page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../services/api';
 
 import './Main.css';
 import logo from '../assets/logotipo.svg';
@@ -6,75 +9,74 @@ import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
 
 export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id
+                }
+            });
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: {
+                user: match.params.id
+            }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: {
+                user: match.params.id
+            }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         <div className="main-container">
-            <img src={logo} alt="tindev" />
-            <ul>
-                <li>
-                    <img src="https://abrilexame.files.wordpress.com/2019/11/eletrobras.jpg?quality=70&strip=info&resize=680,453" alt="" />
-                    <footer>
-                        <strong>Docarri</strong>
-                        <p>Studying Techs</p>
-                    </footer>
+            <Link to="/">
+                <img src={logo} alt="tindev" />
+            </Link>
+            {users.length > 0 ? (
+                <ul>
+                    {
+                        users.map(user => (
+                            <li key={user._id}>
+                                <img src={user.avatar} alt={user.name} />
+                                <footer>
+                                    <strong>{user.name}</strong>
+                                    <p>{user.bio}</p>
+                                </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://abrilexame.files.wordpress.com/2019/11/eletrobras.jpg?quality=70&strip=info&resize=680,453" alt="" />
-                    <footer>
-                        <strong>Docarri</strong>
-                        <p>Studying Techs epwfwep pocewofkwefpo wpveoekwvpokv</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://abrilexame.files.wordpress.com/2019/11/eletrobras.jpg?quality=70&strip=info&resize=680,453" alt="" />
-                    <footer>
-                        <strong>Docarri</strong>
-                        <p>Studying Techs</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://abrilexame.files.wordpress.com/2019/11/eletrobras.jpg?quality=70&strip=info&resize=680,453" alt="" />
-                    <footer>
-                        <strong>Docarri</strong>
-                        <p>Studying Techs</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-            </ul>
+                                <div className="buttons">
+                                    <button type="button" onClick={() => handleDislike(user._id)}>
+                                        <img src={dislike} alt="Dislike" />
+                                    </button>
+                                    <button type="button" onClick={() => handleLike(user._id)}>
+                                        <img src={like} alt="Like" />
+                                    </button>
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            ) : (
+                    <div className="empty">Acabou :(</div>
+                )}
         </div>
     );
 }
