03f1474db8dca25b2759be001574a172a40ad70b
Ricardo Balbi <rbalbi@gmail.com>
2020-02-07 21:10:55 -0300

    feat(main): add style to main page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './Main.css';
 import logo from '../assets/logotipo.svg';
 import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
@@ -29,7 +30,7 @@ export default function Main({ match }) {
                     <img src="https://abrilexame.files.wordpress.com/2019/11/eletrobras.jpg?quality=70&strip=info&resize=680,453" alt="" />
                     <footer>
                         <strong>Docarri</strong>
-                        <p>Studying Techs</p>
+                        <p>Studying Techs epwfwep pocewofkwefpo wpveoekwvpokv</p>
                     </footer>
 
                     <div className="buttons">
