54e36d9f4147fb1876474a4c76b5cde994d0d216
Ricardo Balbi <rbalbi@gmail.com>
2020-02-09 15:18:39 -0300

    feat(mobile): add login page

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import Logo from '../assets/logo.png';
+
+export default function Login() {
+    return (
+        <KeyboardAvoidingView
+            behavior="padding"
+            enabled={Platform.OS === 'ios'}
+            style={styles.container}
+        >
+            <Image source={Logo} />
+
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Digite o seu usuário no Github"
+                placeholderTextColor="#999"
+                style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    },
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#df4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
